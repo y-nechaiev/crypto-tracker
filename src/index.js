1afd3b36b32f545f7ef93b1f097f51a996a72344
Yurii Nechaiev <nechaiev.it@gmail.com>
2023-06-09 02:45:49 +0300

    The whole project in one commit :)

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />,
+);
